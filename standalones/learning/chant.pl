36e962846f17c888d704ffc80b2ca15cc326c528
dylithian <steffler.rm@gmail.com>
2020-07-31 15:59:36 -0600

    Meditated on the prayer.

--- standalones/learning/chant.pl
+++ standalones/learning/chant.pl
@@ -3,6 +3,7 @@
 # I'm not a superstitous man, but if there's any god in this universe I just know he'd pull this kind of stuff.
 
 # Not sure why this is in comments, but I'm putting it here since it was in the text. Is it just a helpful header to tell people how to run the program?
+# Apparently it's a Unix header to enable warnings.
 #!/usr/bin/perl -w
 print "Hello, world.\n";
 print "I PRAY TO THE PROGRAMMING GODS!\n";      # Not in the tutorial, but it's here for good measure.
