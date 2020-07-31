c3122139995888658441581dac48228b0cd8916a
dylithian <steffler.rm@gmail.com>
2020-07-31 16:08:19 -0600

    Comment spacing

--- standalones/learning/chant.pl
+++ standalones/learning/chant.pl
@@ -1,4 +1,4 @@
-#https://docs.google.com/viewer?url=https%3A%2F%2Fblob.perl.org%2Fbooks%2Fbeginning-perl%2F3145_Chap01.pdf
+# https://docs.google.com/viewer?url=https%3A%2F%2Fblob.perl.org%2Fbooks%2Fbeginning-perl%2F3145_Chap01.pdf
 # I'm writing this because apparently according to the text, I need to write this script in order to perform proper tribute to the programming gods and ensure I truly learn Perl.
 # I'm not a superstitous man, but if there's any god in this universe I just know he'd pull this kind of stuff.
 
