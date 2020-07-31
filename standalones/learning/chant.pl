6b692b87669539de316d884d2ff7a6a8512e08aa
dylithian <steffler.rm@gmail.com>
2020-07-31 15:58:14 -0600

    Just a prayer, nothing to see here.

---
+++ standalones/learning/chant.pl
@@ -0,0 +1,8 @@
+#https://docs.google.com/viewer?url=https%3A%2F%2Fblob.perl.org%2Fbooks%2Fbeginning-perl%2F3145_Chap01.pdf
+# I'm writing this because apparently according to the text, I need to write this script in order to perform proper tribute to the programming gods and ensure I truly learn Perl.
+# I'm not a superstitous man, but if there's any god in this universe I just know he'd pull this kind of stuff.
+
+# Not sure why this is in comments, but I'm putting it here since it was in the text. Is it just a helpful header to tell people how to run the program?
+#!/usr/bin/perl -w
+print "Hello, world.\n";
+print "I PRAY TO THE PROGRAMMING GODS!\n";      # Not in the tutorial, but it's here for good measure.
